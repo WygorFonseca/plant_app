e7e8f2273acf12e5e09d6ae6d3aee1dc6c72e01b
unknown <wygorfonseca@hotmail.com>
2021-01-09 13:37:15 -0300

    HomePage - Título com botão de mais

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -28,6 +28,83 @@ class Body extends StatelessWidget {
       child: Column(
         children: [
           HeaderWithSearch(size: size),
+          TitleWithMoreButton()
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithMoreButton extends StatelessWidget {
+  const TitleWithMoreButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          TextWithCustomUnderline(text: 'Recomendado'),
+          FlatButton(
+            onPressed: () {},
+            color: Theme.of(context).primaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0)
+            ),
+            child: Text(
+              'Mais',
+              style: TextStyle(
+                color: Colors.white
+              ),
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TextWithCustomUnderline extends StatelessWidget {
+  const TextWithCustomUnderline({
+    Key key,
+    this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 5.0
+            ),
+            child: Text(
+              text,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              height: 7.0,
+              margin: EdgeInsets.only(
+                right: 5.0
+              ),
+              color: Theme.of(context).primaryColor.withOpacity(0.2),
+            ),
+          )
         ],
       ),
     );
